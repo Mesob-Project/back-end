a5e0ec83f213f14eed91101de6259e28eabe0020
bisratlike <bisratlike@gmail.com>
2023-12-04 22:44:02 +0300

    setting up the back-end

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/selfservice', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+// Define your MongoDB schema and models here
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
