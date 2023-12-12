d667a49797d979af33e978af634577b3159768be
bisratlike <bisratlike@gmail.com>
2023-12-12 23:24:15 +0300

    route  and control

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+// // routes/userRoutes.js
+
+// const express = require('express');
+// const router = express.Router();
+// const userController = require('../controllers/userController');
+
+// // Define routes using the userController functions
+// router.post('/register', userController.registerUser);
+// router.post('/login', userController.loginUser);
+// // Other user-related routes...
+
+// module.exports = router;
